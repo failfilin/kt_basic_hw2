1d282340dd12ffa4d64069163d4bd1192ff9066e
Failfilin <apseliverstov@yandex.ru>
2023-05-07 14:32:58 +0300

    Change in if case

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,4 @@
 fun main() {
-    val likes = 235
-    if (likes != 11 && likes % 10 == 1) println("Понравилось $likes человеку") else println("Понравилось $likes людям")
+    val likes = 1212
+    if (likes % 100 != 11 && likes % 10 == 1) println("Понравилось $likes человеку") else println("Понравилось $likes людям")
 }
